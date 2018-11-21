083f5db38caaa1d9717883acae6e28886edf1fb7
Luis  Senior <heybluguy@yahoo.com>
2018-11-21 00:54:17 -0600

    Dragon TDD.

--- mythical-creatures/lib/dragon.rb
+++ mythical-creatures/lib/dragon.rb
@@ -1,9 +1,28 @@
 class Dragon
   def initialize(name, color, rider)
     @name = name
+    @rider = rider
+    @color = color
+    @eat = 0
   end
 
   def name
-    @name 
+    @name
+  end
+
+  def rider
+    @rider
+  end
+
+  def color
+    @color
+  end
+
+  def hungry?
+    @eat >= 3 ? false : true
+  end
+
+  def eat
+    @eat += 1
   end
 end
