16e55af74d9e7178d0a91dbc97214f30446f50fe
Luis  Senior <heybluguy@yahoo.com>
2018-11-20 21:13:17 -0600

    basics.

--- problem-solving/test/simple_math_test.rb
+++ problem-solving/test/simple_math_test.rb
@@ -4,6 +4,7 @@ require "./lib/simple_math"
 class SimpleMathTest < Minitest::Test
 
   def test_it_can_add_two_numbers
+    # skip
     sm = SimpleMath.new
 
     result = sm.add(2,2)
@@ -13,6 +14,7 @@ class SimpleMathTest < Minitest::Test
   end
 
   def test_it_can_add_two_different_numbers
+    # skip
     sm = SimpleMath.new
 
     result = sm.add(5,3)
@@ -22,6 +24,7 @@ class SimpleMathTest < Minitest::Test
   end
 
   def test_it_can_subtract_two_numbers
+    # skip
     sm = SimpleMath.new
 
     result = sm.subtract(10,2)
@@ -31,6 +34,7 @@ class SimpleMathTest < Minitest::Test
   end
 
   def test_it_can_subtract_two_other_numbers
+    # skip
     sm = SimpleMath.new
 
     result = sm.subtract(50,27)
@@ -40,32 +44,40 @@ class SimpleMathTest < Minitest::Test
   end
 
   def test_it_can_multiply_two_numbers
+    # skip
+    sm = SimpleMath.new
+    result = sm.multiply(2, 3)
+    expected = 6
 
-    # instantiate the class
-    #
-    # call the method being tested
-    # determine your expected result
-    #
-    # check results
+    assert_equal expected, result
   end
 
 
   def test_it_can_multiply_two_other_numbers
+    # skip
+    sm = SimpleMath.new
+    result = sm.multiply(5,5)
+    expected = 25
 
-    # instantiate the class
-    #
-    # call the method being tested
-    # determine your expected result
-    #
-    # check results
+    assert_equal expected, result
   end
 
   def test_it_can_divide_two_numbers
-    # your code goes here
+    # skip
+    sm = SimpleMath.new
+    result = sm.divide(25,5)
+    expected = 5
+
+    assert_equal expected, result
   end
 
   def test_it_can_divide_two_other_numbers
-    # # your code goes here
+    # skip
+    sm = SimpleMath.new
+    result = sm.divide(5,1)
+    expected = 5
+
+    assert_equal expected, result
   end
 
 end
