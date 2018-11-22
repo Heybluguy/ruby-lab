9d8c0acb4e874bf87f122fe44ab36aebce7ef3c1
Luis  Senior <heybluguy@yahoo.com>
2018-11-22 13:24:52 -0500

    Hobbit TDD

--- mythical-creatures/lib/hobbit.rb
+++ mythical-creatures/lib/hobbit.rb
@@ -1,7 +1,29 @@
 class Hobbit
-  def initialize(name)
-
+  attr_reader :name, :age, :disposition
+  
+  def initialize(name, disposition = "homebody")
+    @name = name
+    @disposition = disposition
+    @age = 0
   end
 
+  def celebrate_birthday
+    @age += 1
+  end
 
+  def adult?
+    @age <= 32 ? false : true
+  end
+
+  def old?
+    @age >= 101 ? true : false
+  end
+
+  def has_ring?
+    @name == 'Frodo' ? true : false
+  end
+
+  def is_short?
+    true
+  end
 end
